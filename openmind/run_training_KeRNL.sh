0bbff2c300e8a7c168f434f4323c56e36243960d
ehoseini <ehoseini@mit.edu>
2019-01-31 10:46:01 -0500

    adding bash script to run on openmind

--- openmind/run_training_KeRNL.sh
+++ openmind/run_training_KeRNL.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
-#SBATCH --output=Reconstruct3D_AllFrames_Vertical_CPU_V2.out
+#SBATCH --output=run_training_KeRNL.out
 #SBATCH -c 12
 #SBATCH --ntask=1
 #SBATCH --mem-per-cpu 15000
-#SBATCH --time=05-00:00:00
-#SBATCH --array=1-500%100
+#SBATCH --time=00-01:00:00
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 
+#singularity shell docker://tensorflow/tensorflow:1.12.0-py3
 
-module add mit/matlab/2017a
-matlab -nodisplay -signelCompThread -r "addpath(genpath('~/MyCodes/DualLensLightField/')); \
-recordingDay='12_15_17_40X_worm';\
-VideoNumber='video 1';\
-Direction='horizontal';\
-data_folder=['/om/project/boyden/DualLensLightField/',recordingDay];cd(data_folder); \
-InputPath=[data_folder,'/',VideoNumber,'/',Direction];\
-PSFPath=[data_folder,'/psf'];\
-FirstFramePath=[InputPath,'/Reconstructed'];\
-SavePath=[data_folder,'/',VideoNumber,'/',Direction];\
-Reconstruction3D_script_Frame2ToEnd(InputPath,PSFPath,SavePath,$SLURM_ARRAY_TASK_ID,FirstFramePath);\
-quit;"
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/simg_images
+singularity exec -B /om:/om /om/user/`whoami`/simg_images/tensorflow-1.12.0-py3.simg /usr/bin/python -c "import tensorflow as tf"
