1e59e94967103ded40341c9b345905c5af6942f1
Eghbal Hosseini <ehoseini@openmind7.mit.edu>
2019-02-11 16:50:37 -0500

    fixed om_gpu_code

--- openmind/om_gpu_run_train_KeRNL_v2_VS_BPTT_MNIST_with_args.sh
+++ openmind/om_gpu_run_train_KeRNL_v2_VS_BPTT_MNIST_with_args.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=om_train_KeRNL
 #SBATCH --mem=20000
 #SBATCH --gres= tesla-k20:1
-#SBATCH --time=60:00:00
+#SBATCH --time=1:00:00
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
@@ -10,5 +10,5 @@
 #SBATCH --output=om_result_gpu_train_KeRNL_v2_VS_BPTT_MNIST_%a.out
 
 module add openmind/singularity
-export SINGULARITY_CACHEDIR=/om/user/`whoami`/simg_images
-singularity exec --nv -B /om:/om /om/user/`whoami`/st/om_image_tensorflow1-12_gpu.simg /usr/bin/python ~/MyCodes/KeRNL/om_train_keRNL_v2_VS_BPTT_MNIST_with_args.py $SLURM_ARRAY_TASK_ID
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+singularity exec --nv -B /om:/om /om/user/`whoami`/st/om_image_tensorflow1-12_gpu.simg /usr/bin/python3 ~/MyCodes/KeRNL/om_train_keRNL_v2_VS_BPTT_MNIST_with_args.py $SLURM_ARRAY_TASK_ID
