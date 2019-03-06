c8af9fc4c31a98b2388ccb80e66a5eb46552c660
eghbal-linux <eghbal.hosseini@gmail.com>
2019-03-06 10:13:32 -0500

    added parameters

--- om_train_gpu_kernl_rnn_adding_dataset.py
+++ om_train_gpu_kernl_rnn_adding_dataset.py
@@ -63,11 +63,11 @@ num_hidden = 100 # hidden layer num of features
 num_output = 1 # value of the addition estimation
 #
 # Noise Parameters
-perturbation_std=1e-5
+perturbation_std=1e-8
 
 #
 # save dir
-log_dir = "/om2/user/ehoseini/MyData/KeRNL/logs/kernl_rnn_addition_dataset/tanh_add_eta_weight_%1.0e_batch_%1.0e_hum_hidd_%1.0e_gc_%1.0e_steps_%1.0e_run_%s" %(weight_learning_rate,batch_size,num_hidden,grad_clip,training_steps, datetime.now().strftime("%Y%m%d_%H%M"))
+log_dir = "/om2/user/ehoseini/MyData/KeRNL/logs/kernl_rnn_addition_dataset/add_T_%1.0e_tanh_add_eta_W_%1.0e_eta_T_%1.0e_Noise_%1.0e_batch_%1.0e_hum_hidd_%1.0e_gc_%1.0e_steps_%1.0e_run_%s" %(time_steps,weight_learning_rate,tensor_learning_rate,perturbation_std,batch_size,num_hidden,grad_clip,training_steps, datetime.now().strftime("%Y%m%d_%H%M"))
 log_dir
 
 # create a training and testing dataset
