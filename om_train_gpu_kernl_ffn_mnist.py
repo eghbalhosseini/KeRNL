42048da867bf0268688dd35f0fb2649fec015fe2
eghbal-linux <eghbal.hosseini@gmail.com>
2019-04-06 16:52:11 -0400

    bugfix

--- om_train_gpu_kernl_ffn_mnist.py
+++ om_train_gpu_kernl_ffn_mnist.py
@@ -41,6 +41,18 @@ from tensorflow.contrib import slim
 import kernl_rnn_cell
 
 # uplading mnist data
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+train_data = mnist.train.images  # Returns np.array
+train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+eval_data = mnist.test.images  # Returns np.array
+eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+tf.logging.set_verbosity(old_v)
 
 
 # Training Parameters
